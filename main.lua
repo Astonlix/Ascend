0fa27bc3b85343074c883a96811f2a4e17722a76
vurelix <107379616+Astonlix@users.noreply.github.com>
2024-03-15 23:43:48 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 4924922222 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonlix/eggHub/main/brookhaven.lua"))()
+elseif game.PlaceId == 3101667897 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonlix/eggHub/main/legendOfSpeed.lua"))()
 end
