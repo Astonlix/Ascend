a6dd52178f6ac3dd6d746039c1ebcf2a181a5088
vurelix <107379616+Astonlix@users.noreply.github.com>
2024-03-15 23:27:01 -0500

    Create brookhaven.lua

---
+++ brookhaven.lua
@@ -0,0 +1,31 @@
+local LocalPlayer = game.Players.LocalPlayer
+local Easter = game.Workspace["Easter001!"]
+local RobloxSign = game.Workspace["RobloxSign001!"]
+local HRP = LocalPlayer.Character.HumanoidRootPart
+local function activationCheck()
+    if Easter:FindFirstChild("RobloxEggHunt") then
+        return true
+    else
+        HRP.CFrame = CFrame.new(RobloxSign.Model.Roblox.Position - Vector3.new(0, 10, 0))
+        fireclickdetector(RobloxSign.Model.Roblox.ClickDetector)
+        task.wait(1)
+        if Easter:FindFirstChild("RobloxEggHunt") then
+            return true
+        else
+            return false
+        end
+    end
+end
+
+if activationCheck() then
+    while activationCheck() and task.wait() do
+        for i, v in ipairs(Easter.RobloxEggHunt:GetChildren()) do
+            if v.Name == "Eggy" and v.Transparency == 0 then
+                HRP.CFrame = CFrame.new(v.Position + Vector3.new(0, 1, 0))
+            end
+        end
+    end
+else
+    print("Its not fucking activated what the fuck are you doing you retard")
+    activationCheck()
+end
