e971d23feb888bd76f074e1846d88d79f1d24e3a
vurelix <107379616+Astonlix@users.noreply.github.com>
2024-03-16 05:43:11 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 4924922222 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonlix/eggHub/main/brookhaven.lua"))()
 elseif game.PlaceId == 3101667897 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonlix/eggHub/main/legendsOfSpeed.lua"))()
+elseif game.PlaceId == 6766156863 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonlix/eggHub/main/strongman-simulator.lua"))()
 end
